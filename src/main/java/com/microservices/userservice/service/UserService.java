6ceaa8df31080b3fffafba4e7ace92747303fcb0
yasithwimukthi <wimukthiyasith@gmail.com>
2026-04-09 22:52:20 +0530

    feature : implement global error handling

--- src/main/java/com/microservices/userservice/service/UserService.java
+++ src/main/java/com/microservices/userservice/service/UserService.java
@@ -6,10 +6,12 @@ import com.microservices.userservice.entity.User;
 import com.microservices.userservice.mapper.UserMapper;
 import com.microservices.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -19,9 +21,13 @@ public class UserService {
 
     public UserResponse create(UserRequest request){
 
+        log.info("Creating user with email {}", request.getEmail());
+
         User user = mapper.toEntity(request);
 
         repository.save(user);
+
+        log.info("User created with id {}", user.getId());
 
         return mapper.toResponse(user);
     }
